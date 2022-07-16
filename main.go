205cab4ae629c3fa17918e0f831a65cb532aaa8d
Hust-zh <2388594514@qq.com>
2022-07-16 10:17:09 +0800

    v2

--- main.go
+++ main.go
@@ -23,3 +23,7 @@ func main() {
 	// Perform the search for the specified term.
 	search.Run("president")
 }
+func main() {
+	// Perform the search for the specified term.
+	search.Run("president")
+}
